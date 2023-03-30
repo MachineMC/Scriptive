48588b2e1279286bdde47d43b4adf641b462985a
_tud <mmbakkar06@gmail.com>
2023-03-30 11:21:30 +0300

    Add publishing to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    java
+    `maven-publish`
 }
 
 group = "org.machinemc"
@@ -7,15 +8,43 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("http://www.machinemc.org/releases")
+        isAllowInsecureProtocol = true
+    }
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:20.1.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    compileOnly("org.jetbrains:annotations:23.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+publishing {
+    repositories {
+        maven {
+            name = "machine"
+            url = uri("http://www.machinemc.org/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.machinemc"
+            artifactId = "scriptive"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 }
