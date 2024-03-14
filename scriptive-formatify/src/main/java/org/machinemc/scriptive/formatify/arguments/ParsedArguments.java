6432f2f1b4cdc6b9fd95a9aed76182766d6e243b
Pesek <jakub.zahomolkou@gmail.com>
2024-03-14 15:43:58 +0100

    unused import

--- scriptive-formatify/src/main/java/org/machinemc/scriptive/formatify/arguments/ParsedArguments.java
+++ scriptive-formatify/src/main/java/org/machinemc/scriptive/formatify/arguments/ParsedArguments.java
@@ -3,7 +3,6 @@ package org.machinemc.scriptive.formatify.arguments;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
-import java.util.Optional;
 
 public class ParsedArguments {
 
