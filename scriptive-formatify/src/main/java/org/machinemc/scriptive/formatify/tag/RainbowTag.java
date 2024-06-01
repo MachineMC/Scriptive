48a14a94a5eb322c54368e3b60f228fd0650bac9
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2024-06-01 15:54:21 +0300

    Use hexadecimal int instead of string for rainbow hex colors

--- scriptive-formatify/src/main/java/org/machinemc/scriptive/formatify/tag/RainbowTag.java
+++ scriptive-formatify/src/main/java/org/machinemc/scriptive/formatify/tag/RainbowTag.java
@@ -5,13 +5,13 @@ import org.machinemc.scriptive.style.HexColor;
 
 public class RainbowTag extends GradientTag {
 
-    public static final Colour RED = new HexColor("e81416");
-    public static final Colour ORANGE = new HexColor("ffa500");
-    public static final Colour YELLOW = new HexColor("faeb36");
-    public static final Colour GREEN = new HexColor("79c314");
-    public static final Colour BLUE = new HexColor("487de7");
-    public static final Colour INDIGO = new HexColor("4b369d");
-    public static final Colour VIOLET = new HexColor("70369d");
+    public static final Colour RED = new HexColor(0xE81416);
+    public static final Colour ORANGE = new HexColor(0xFFA500);
+    public static final Colour YELLOW = new HexColor(0xFAEB36);
+    public static final Colour GREEN = new HexColor(0x79C314);
+    public static final Colour BLUE = new HexColor(0x487DE7);
+    public static final Colour INDIGO = new HexColor(0x4B369D);
+    public static final Colour VIOLET = new HexColor(0x70369D);
 
     public RainbowTag(double offset) {
         super(new Colour[]{
